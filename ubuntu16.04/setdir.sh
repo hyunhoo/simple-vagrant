69a5d826dbf235080d876e8fa9ee212eea05f75f
hyunhoo <hyunhoolee7@gmail.com>
2020-01-29 09:55:23 +0900

    Initial commit

---
+++ ubuntu16.04/setdir.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for var in "$@"
+do
+    mkdir "$var"
+done
---
+++ ubuntu16.04/sys-prereqs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -x
+
+sudo apt update
+
+# Install git
+sudo apt install -y git
+
+# Install docker
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt update
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Install golang
+sudo apt install -y golang-go
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+export NVM_DIR="${HOME}/.nvm"
+[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"
+[ -s "${NVM_DIR}/bash_completion" ] && . "${NVM_DIR}/bash_completion"
+
+nvm install 8
+nvm use 8
